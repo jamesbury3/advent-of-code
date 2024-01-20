f19de92a9ce16ed8dac48638583537fc2d0c8465
jamesbury3 <jamesbury77@gmail.com>
2024-01-20 14:18:25 -0500

    This is the template moving forward

--- src/Day1.java
+++ src/Day1.java
@@ -1,6 +1,10 @@
+import day1.Node;
+
 class Day1 extends Day {
 
 	public void solve() {
+		Node n = new Node();
+		n.print();
 		info("Solved!");
 	}
 }
---
+++ src/day1/Node.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Node {
+
+	public void print() {
+		System.out.println("I'm a node!");
+	}
+}
